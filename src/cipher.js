56ba53e417d99a9337eb1590304529ff555163a1
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-02-11 16:13:02 -0300

    primera prueba de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,37 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (offset, text) => {
+
+    let textAsciiEncode  = '';
+
+
+  for (let i = 0; i < text.length; i++) {
+    let textAscii = text.charCodeAt(i);
+
+    if(textAscii >= 65 && textAscii <=90){
+      textAscii = ((textAscii - 65 + parseInt(offset)))%26 + 65;
+
+    }
+textAsciiEncode += String.fromCharCode(textAscii);
+  }
+  
+  return textAsciiEncode;
+},
+
+
+  decode: (offset,text) => {
+
+    let textDeco = '';
+    let textAscii = texts.charCodeAt (i);
+
+    for (let i = 0; i < text.length; i++){
+
+      if(textAscii >= 65 && textAscii <=90){
+        textAscii = (( textAscii - 90 - parseInt (offset))) % 26 + 90;
+
+      }
+
+      textDeco += String.fromCharCode(textAscii);
+    }
+    return textDeco;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-/* Acá va tu código */
+
+const botonEncode = document.getElementById('encode');
+const casa = document.getElementById('home');
+const instrucciones = document.getElementById('instruction');
+const casacodifi = document.getElementById('homeEncode');
+const casaDecode = document.getElementById("decoForm")
+
+botonEncode.addEventListener('click', (evento) => {
+ casa.style.display= "none";
+ casaDecode.style.display= "none;"
+ instrucciones.style.display= "block";
+ casacodifi.style.display= "block";
+});
+
+const botonDecode = document.getElementById('decode');
+
+botonDecode.addEventListener('click',(evento) => {
+
+  casa.style.display= "none";
+  casacodifi.style.display= "none";
+  instrucciones.style.display= "block";
+  casaDecode.style.display= "block";
+});
+
+const botonVolver = document.getElementById('backHome');
+
+botonVolver.addEventListener('click', (evento) => {
+
+  casa.style.display= "block";
+  instrucciones.style.display= "none";
+  casaDecode.style.display= "none";
+  casacodifi.style.display= "none";
+});
+
+
+const offset = document.getElementById('offset');
+const button_enter = document.getElementById('goEncode');  
+const contenedor = document.getElementById('contendedor_datos');
+ 
+  
+  button_enter.addEventListener('click', (evento) => {
+    
+    let encodvr = document.getElementById('text').value;
+    let offset = document.getElementById('offset').value;
+
+    document.getElementById("contenedor_datos").innerHTML = (`${encodvr} ${offset}` );
+
+
+  })
+  
+ 
+
+  
+  
+
+
+
+ 
